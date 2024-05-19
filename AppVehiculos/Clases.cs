cfe1e7417b54ae40006bd42edf216352a77ae54e
Mirco Sair Salcedo Ataulluco <mirkosair28@gmail.com>
2024-05-19 14:52:43 -0500

    Creamos espacio de nombres (clases)
    Crearemos una clase abstracta, la cual no se puede instanciar directamente, además sus clases derivadas cAuto y cVehículo, las cuales usaremos para llenar y trabajar en un array.

---
+++ AppVehiculos/Clases.cs
@@ -0,0 +1,125 @@
+namespace EspacioDeNombres
+{
+    public abstract class cVehiculo
+    {
+        private string aPlaca;
+        private string aMarca;
+        private string aModelo;
+
+        public cVehiculo()
+        {
+            aPlaca = "";
+            aMarca = "";
+            aModelo = "";
+        }
+        public cVehiculo(string pPlaca, string pMarca, string pModelo)
+        {
+            aPlaca = pPlaca;
+            aMarca = pMarca;
+            aModelo = pModelo;
+        }
+        // Propiedades
+        public string Placa
+        {
+            get { return aPlaca; }
+            set { aPlaca = value; }
+        }
+        public string Marca
+        {
+            get { return aMarca; }
+            set { aMarca = value; }
+        }
+        public string Modelo
+        {
+            get { return aModelo; }
+            set { aModelo = value; }
+        }
+        // Metodos
+        public virtual void Leer()
+        {
+            Console.Write("Placa:");
+            aPlaca = Console.ReadLine();
+            Console.Write("Marca:");
+            aMarca = Console.ReadLine();
+            Console.Write("Modelo:");
+            aModelo = Console.ReadLine();
+        }
+        public virtual void Mostrar()
+        {
+            Console.WriteLine($"Placa: {aPlaca}");
+            Console.WriteLine($"Placa: {aMarca}");
+            Console.WriteLine($"Placa: {aModelo}");
+        }
+        public abstract int CalcularPeaje()
+        {
+            
+        }
+
+        public override bool Equals(object? vehiculo)
+        {
+            if (aPlaca == ((cVehiculo)vehiculo).Placa)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }   
+    }
+
+    public class cAuto : cVehiculo
+    {
+        private int aAnio;
+
+        // Constructores
+        public cAuto() : base() 
+        {
+            aAnio = 1886;
+        }
+
+        public cAuto(string pPlaca, string pMarca, string pModelo, int pAnio) : base(pPlaca, pMarca, pModelo)
+        {
+            if (pAnio >= 1886)
+            {
+                aAnio = pAnio;
+            }
+            else 
+            {
+                aAnio = 1886;
+            }
+        }
+
+        // Propiedades
+        public int Anio
+        {
+            get { return aAnio; }
+            set
+            {
+                if (value >= 1886)
+                {
+                    aAnio = value;
+                }
+            }
+        }
+        // Metodos
+        public override int CalcularPeaje()
+        {
+            int anioActual = DateTime.Now.Year;
+            int Antiguedad = anioActual - aAnio;
+            if (Antiguedad > 20)
+            {
+                return 20;
+            }
+            else if (Antiguedad > 15)
+            {
+                return 10;
+            }
+            else (Antiguedad)
+            {
+                return 0;
+            }
+                
+        }
+    }
+}
