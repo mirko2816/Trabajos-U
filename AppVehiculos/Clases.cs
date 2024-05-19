1a72a50d7ae77b3a4ff43070a33ced90b6a485f2
Mirco Sair Salcedo Ataulluco <mirkosair28@gmail.com>
2024-05-19 13:57:43 -0500

    AppVehiculos
    Es una aplicacion de consola que puede agregar objetos (vehiculos) a un array ademas de otros objetos derivados aplicando herencia y polimorfismo. Programado en c#

---
+++ AppVehiculos/Clases.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace EspacioDeNombres
+{
+	public class cVehiculo
+	{
+		// ATRIBUTOS
+		string aPlaca;
+		string aMarca;
+
+        // CONSTRUCTORES
+        public cVehiculo()
+		{
+		}
+
+		
+
+		// PROPIEDADES
+
+		// METODOS
+	}
+
+	public class cAuto : cVehiculo
+	{
+		// CONSTRUCTORES
+
+		// ATRIBUTOS
+
+		// PROPIEDADES
+
+		// METODOS
+	}
+
+	public class cCamion : cVehiculo
+	{
+		// CONSTRUCTORES
+
+		// ATRIBUTOS
+
+		// PROPIEDADES
+
+		// METODOS
+	}
+}
---
+++ AppVehiculos/Program.cs
@@ -0,0 +1,18 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime fecha = DateTime.Now;
+        double precio = 12.33;
+
+        string texto1 = string.Format("Costo: {0:C}", precio );
+        string texto2 = string.Format("Fecha: {0:dddd}", fecha);
+        
+        Console.WriteLine(texto1);
+        Console.WriteLine(texto2);
+    }
+
+}
+
+
