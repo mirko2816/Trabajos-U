fb7583f1e683c6e0075415f40ee9decb463cfaf4
Mirco <mirkosair28@gmail.com>
2024-06-08 00:49:16 -0500

    Add ListIterativa

---
+++ BibliotecaTDA/ListaIterativa/cListaIterativa.cs
@@ -0,0 +1,69 @@
+﻿namespace BibliotecaTDA;
+
+public class cListaIterativa
+{
+    // Atributos
+    private cNodo aPrimerNodo;
+    private int aNroElementos;
+
+    // Constructores
+    public cListaIterativa()
+    {
+        aPrimerNodo = new cNodo();
+        aNroElementos = 0;
+    }
+
+    public cListaIterativa(cNodo pPrimerNodo)
+    {
+        aPrimerNodo = pPrimerNodo;
+    }
+
+    public cListaIterativa(cNodo pPrimerNodo, int pNroElementos)
+    {
+        aPrimerNodo = pPrimerNodo;
+        aNroElementos = pNroElementos;
+    }
+
+    // Propiedades
+    public cNodo PrimerNodo
+    {
+        get {return aPrimerNodo;}
+        set {aPrimerNodo = value;}
+    }
+
+    public int NumeroElementos
+    {
+        get {return aNroElementos;}
+    }
+
+    // Metodo
+    public bool EsVacio()
+    {
+        return aPrimerNodo == null;
+    }
+
+    public void Listar()
+    {
+        // Recorre nodos y procesa cada elemento
+        cNodo nodoAux = aPrimerNodo;
+        while (nodoAux != null)
+        {
+            Console.WriteLine(nodoAux.Informacion.ToString());
+            nodoAux = nodoAux.Siguiente;
+        }
+    }
+
+    public object Iesimo(int indice)
+    {
+        cNodo nodoAux = aPrimerNodo;
+        int i = 0;
+
+        while (i != indice)
+        {
+            nodoAux = nodoAux.Siguiente;
+            i++;
+        }
+
+        return nodoAux;
+    }
+}
