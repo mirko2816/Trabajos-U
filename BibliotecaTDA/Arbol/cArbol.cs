e5a095bdb1053391daa43a0ac74ac727f88419ee
Mirco Sair Salcedo <78182502+mirko2816@users.noreply.github.com>
2024-07-04 22:14:34 +0000

    cArbol avanzado

---
+++ BibliotecaTDA/Arbol/cArbol.cs
@@ -0,0 +1,28 @@
+﻿namespace BibliotecaTDA;
+
+public class cArbol
+{
+    // Atributos 
+    private object aRaiz;
+    private cArbol aHijoIzq;
+    private cArbol aHermano;
+
+    // Constructores
+    public cArbol()
+    {
+        aRaiz = null;
+        aHijoIzq = null;
+        aHermano = null;        
+    }
+
+    public cArbol(object pInfo)
+    {
+        aRaiz = pInfo;
+        aHijoIzq  = null;
+        aHermano = null;
+    }
+
+    // Metodos 
+
+
+}
