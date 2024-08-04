eb13c724948e55547a6e61c4569dea1db55c03ab
Mirco <mirkosair28@gmail.com>
2024-08-04 13:49:52 -0500

    cNodo para lista doble creada

---
+++ BibliotecaTDA/cNodoListaDoble.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaTDA
+{
+    public class cNodoListaDoble
+    {
+        //========== ATRIBUTOS ==========
+        private cNodoListaDoble aNodoAnterior;
+        private object aElemento;
+        private cNodoListaDoble aNodoPosterior;
+
+        // ===================== METODOS ==================
+
+        // ========CONSTRUCTORES =========
+        public cNodoListaDoble()
+        {
+            aNodoAnterior = null;
+            aElemento = null;
+            aNodoPosterior = null;
+        }
+
+        public cNodoListaDoble(cNodoListaDoble pNodoAnterior, object pElemento, cNodoListaDoble pNodoPosterior)
+        {
+            aNodoAnterior = pNodoAnterior;
+            aElemento = pElemento;
+            aNodoPosterior = pNodoPosterior;
+        }
+
+        public static cNodoListaDoble Crear()
+        {
+            return new cNodoListaDoble();
+        }
+
+        public static cNodoListaDoble Crear(cNodoListaDoble pNodoAnterior, object pElemento, cNodoListaDoble pNodoPosterior)
+        {
+            return new cNodoListaDoble(pNodoAnterior, pElemento, pNodoPosterior);
+        }
+
+        // =========== MODIFICADORES ===========
+        public void ModificarNodoAnterior(cNodoListaDoble pNodoAnterior)
+        {
+            aNodoAnterior = pNodoAnterior;
+        }
+
+        public void ModificarElemento(object pElemento)
+        {
+            aElemento = pElemento;
+        }
+
+        public void ModificarNodoPosterior(cNodoListaDoble pNodoPosterior)
+        {
+            aNodoPosterior = pNodoPosterior;
+        }
+
+        // ============ SELECTORES ==============
+        public cNodoListaDoble NodoAnterior()
+        {
+            return aNodoAnterior;
+        }
+
+        public object Elemento()
+        {
+            return aElemento;
+        }
+
+        public cNodoListaDoble NodoPosterior()
+        {
+            return aNodoPosterior;
+        }
+    }
+}
