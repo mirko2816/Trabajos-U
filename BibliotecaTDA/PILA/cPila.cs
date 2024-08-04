86e6e95dfacf21bed10bccdd0c53d0460b68f847
Mirco <mirkosair28@gmail.com>
2024-08-04 13:34:50 -0500

    cPila completado

--- BibliotecaTDA/PILA/cPila.cs
+++ BibliotecaTDA/PILA/cPila.cs
@@ -2,5 +2,68 @@
 
 public class cPila
 {
+    // Atributos
+    private object aElemento;
+    private cPila aSubPila;
+
+    // Constructores
+    public cPila()
+    {
+        aElemento = null;
+        aSubPila = null;
+    }
+    public cPila(object pElem, cPila pSubPila)
+        : base()
+    {
+        aElemento = pElem;
+        aSubPila = pSubPila;
+    }
+    
+
+    // Metodos de clase
+    public static cPila Crear()
+    {
+        return new cPila();
+    }
+    public static cPila Crear(object pElem, cPila pSub)
+    {
+        return new cPila(pElem, pSub);
+    }
+    
+
+    // Setters y Getters
+    public object Elemento
+    {
+        get { return aElemento; }
+        set { aElemento = value; }
+    }
+    public cPila SubPila
+    {
+        get { return aSubPila; }
+        set { aSubPila = value; }
+    }
+    
+    // Metodos
+    public bool EsVacia()
+    {
+        return (aElemento == null) && (aSubPila == null);
+    }
+    public object Cima()
+    {
+        return aElemento;
+    }
+    public void Apilar(object pElemento)
+    {
+        aSubPila = new cPila(aElemento, aSubPila);
+        aElemento = pElemento;
+    }
+    public void Desapilar()
+    {
+        if (!EsVacia())
+        {
+            aElemento = aSubPila.Elemento;
+            aSubPila = aSubPila.SubPila;
+        }
+    }
 
 }
