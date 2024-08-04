de24066898dfe6b7e7621d9a3c354aedd94b3c7e
Mirco <mirkosair28@gmail.com>
2024-08-04 13:53:53 -0500

    a

--- AppUsoBiblioteca/AppArboles/obj/Debug/net8.0/AppArboles.AssemblyInfo.cs
+++ AppUsoBiblioteca/AppArboles/obj/Debug/net8.0/AppArboles.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppArboles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ede841590b5ef3e3501227faa40bc270280f3eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb13c724948e55547a6e61c4569dea1db55c03ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppArboles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppArboles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ AppUsoBiblioteca/AppGrafos/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grafo;
+
+namespace AppCaminosMInimoss
+{
+    class Program
+    {
+
+        public static void merge_(ref ArrayList arr, int ini, int mid, int fin)
+        {
+            /*
+             * el arreglo esta dividido en 2, 
+             * arr[ini, mid]  arr[mid + 1, fin]
+             * ademas, ambos arreglos estan ordenados
+             */
+            
+            ArrayList aux = new ArrayList();
+
+            for (int m = 0; m < fin - ini + 1; m++)
+            {
+                aux.Add(0);
+            }
+
+            int i = ini;
+            int j = mid + 1;
+            int k = 0;
+            while (i <= mid && j <= fin)
+            {
+                if ((arr[i] as CArista).Peso < (arr[j] as CArista).Peso)
+                {
+                    aux[k++] = arr[i++];
+                }
+                else
+                {
+                    aux[k++] = arr[j++];
+                }
+            }
+            if (i <= mid)
+            {
+                // copiar elementos sobrantes de la izquierda en aux
+                for (int l = i; l <= mid; l++)
+                {
+                    aux[k++] = arr[l];
+                }
+            }
+
+            if (j <= fin)
+            {
+                // copiar elementos sobrantes de derecha en aux 
+                for (int l = j; l <= fin; l++)
+                {
+                    aux[k++] = arr[l];
+                }
+            }
+            // vaciamos los elementos de aux en arr
+            for (int l = 0; l < aux.Count; l++)
+            {
+                arr[ini + l] = aux[l];
+            }
+        }
+        public static void merge_sort_( ArrayList arr, int ini, int fin)
+        {
+            // se garantiza que hay elementos en arr y ademas es dividible
+            if (ini < fin)
+            {
+                int mid = (ini + fin) / 2;
+                merge_sort_(arr, ini, mid);
+                merge_sort_(arr, mid + 1, fin);
+                merge_(ref arr, ini, mid, fin);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+            // -- Crear objeto CaminosMInimos
+            // cCaminosMinimos G = new cCaminosMinimos();
+            // -- Leer CaminosMInimos de archivo
+            // G.LeerDeArchivo("Grafo02.csv");
+            // -- Ejecutar Dijkstra
+            //G.Dijkstra();
+            // -- Mostrar cammino minimo y costo
+            //G.MostrarCaminosMinimos();
+
+            /*
+            // -- Mostrar CaminosMInimos
+            G.Mostrar();
+            */
+            // -- Pausa
+
+
+            // leemos el grafo del archivo
+            cGrafo grafo = new cGrafo();
+            grafo.LeerDeArchivo("grafo.txt");
+            grafo.Mostrar();
+
+            ArrayList lista = new ArrayList();
+
+            for (int i = 0; i < grafo.NroVertices; i++)
+            {
+                for (int j = i; j < grafo.NroVertices; j++)
+                {
+                    if (i != j)
+                        lista.Add(new CArista(i, j, grafo.getPeso(i, j)));
+                }
+            }
+
+
+
+
+            merge_sort_(lista, 0, lista.Count - 1);
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i].ToString());
+            }
+
+
+            ///
+
+            int k = 0;
+            while (k < lista.Count)
+            {
+                // seleccionamos la arista de menor peso
+                CArista ar = (CArista)lista[k];
+            
+                // verifiamos que los extemos i, j no esten en el mismo arbol
+                
+
+                // aplicar DSU  - Union find 
+
+                k = k + 1;
+            }
+            
+            /* construimos nuestra nueva matriz de adyacencia 
+             * para nuestro MST kruskal
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
